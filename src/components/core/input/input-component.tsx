c85f7413d9f4d8b4eecaf9abbefbca05c540236c
Corrie Graham <corriegdev@gmail.com>
2022-06-14 14:00:25 +0200

    update: added custom input type type

--- src/components/core/input/input-component.tsx
+++ src/components/core/input/input-component.tsx
@@ -2,12 +2,14 @@ import React, { InputHTMLAttributes } from 'react';
 import { Render } from '@utils/render';
 import { SInput } from './input-styles';
 
+type TInput = 'text' | 'number' | 'email' | 'password';
 interface IInput extends InputHTMLAttributes<HTMLInputElement> {
     labelLeft?: string;
     labelRight?: string;
+    type?: TInput;
 }
 
-export const Input: React.FC<IInput> = ({ labelLeft = null, labelRight = null, placeholder = '', ...props }): React.ReactElement => {
+export const Input: React.FC<IInput> = ({ labelLeft = null, labelRight = null, placeholder = '', type = 'text', ...props }): React.ReactElement => {
     return (
         <div>
             <Render rif={ !!labelLeft || !!labelRight }>
@@ -16,7 +18,7 @@ export const Input: React.FC<IInput> = ({ labelLeft = null, labelRight = null, p
                     <p>{ labelRight }</p>
                 </div>
             </Render>
-            <SInput placeholder={ placeholder } { ...props }/>
+            <SInput type={ type } placeholder={ placeholder } { ...props }/>
         </div>
     );
 }
