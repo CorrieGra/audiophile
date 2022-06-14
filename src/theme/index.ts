adb3f487f65058ba094bcde2493d523f269fa2ba
Corrie Graham <corriegdev@gmail.com>
2022-06-14 14:00:42 +0200

    update: added colours for light theme

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -1,5 +1,19 @@
 export const theme = {
     light: {
-
+        colours: {
+            orange: {
+                light: '#fbaf85',
+                dark: '#D87D4A',
+            },
+            black: {
+                light: '#101010',
+                dark: '#000000',
+            },
+            gray: {
+                light: '#FAFAFA',
+                dark: '#F1F1F1',
+            },
+            white: '#FFFFFF'
+        }
     }
 }
