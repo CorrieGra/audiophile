fe4418ff74ab3de56c2c2f420d1a7ee0a92999ce
Corrie Graham <corriegdev@gmail.com>
2022-06-14 16:20:34 +0200

    update: added route path to config

--- craco.config.ts
+++ craco.config.ts
@@ -6,7 +6,8 @@ module.exports = {
       '@external': path.resolve(__dirname, '.'),
       '@components': path.resolve(__dirname, 'src/components'),
       '@theme': path.resolve(__dirname, 'src/theme'),
-      '@utils': path.resolve(__dirname, 'src/utils')
+      '@utils': path.resolve(__dirname, 'src/utils'),
+      '@routes': path.resolve(__dirname, 'src/routes'),
     },
   },
 };
