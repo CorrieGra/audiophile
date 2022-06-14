764efe6564d6de91074c24981abafa7d3632414e
Corrie Graham <corriegdev@gmail.com>
2022-06-14 16:21:20 +0200

    update: changed the whole model to contain an array of pages

---
+++ src/components/navigation/navigation-model.ts
@@ -0,0 +1,12 @@
+interface INavigationModel {
+    pages: string[];
+}
+
+export const NavigationModel: INavigationModel = {
+    pages: [
+        'home',
+        'headphones',
+        'speakers',
+        'earphones',
+    ],
+};
